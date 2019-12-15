11a9ff4f42782c8315b9001afb2782531ee8ebb8
Glenn Hope <glenn.alexander.hope@gmail.com>
2019-12-14 20:03:43 -0800

    First real integration test!

--- tests/test_cli_help.rs
+++ tests/test_cli_help.rs
@@ -4,7 +4,7 @@ use assert_cmd::prelude::*;
 use std::process::Command;
 
 #[test]
-fn test_cli() {
+fn test_cli_help() {
     let mut cmd = Command::main_binary().unwrap();
     cmd.arg("--help");
     cmd.assert().success();
---
+++ tests/test_iw_first_network_from_file.rs
@@ -0,0 +1,23 @@
+extern crate assert_cmd;
+extern crate ruwi;
+use assert_cmd::prelude::*;
+use std::process::Command;
+
+#[test]
+fn test_iw_first_network_from_file() {
+    let mut cmd = Command::main_binary().unwrap();
+    cmd.args(&[
+        "-F",
+        "src/samples/iw_two_different_networks.txt",
+        "-c",
+        "print",
+        "-s",
+        "iw",
+        "-A",
+        "first",
+    ]);
+    cmd.assert()
+        .success()
+        .stdout("Valparaiso_Guest_House 2\n")
+        .stderr("[NOTE]: Selected network: \"Valparaiso_Guest_House 2\"\n");
+}
