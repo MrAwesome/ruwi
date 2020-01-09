152b40f321f156c2f5a168f4d46419cbf4c15a53
Glenn Hope <glenn.alexander.hope@gmail.com>
2020-01-08 19:57:38 -0800

    Move parse into its own module

--- tests/test_basic_integration_tests.rs
+++ tests/test_basic_integration_tests.rs
@@ -13,7 +13,7 @@ fn test_cli_help() -> Result<()> {
 #[test]
 fn test_runtime_basic_print() -> Result<()> {
     let mut p = spawn(
-        "./target/debug/ruwi -D -F src/samples/iw_two_different_networks.txt -c print -s iw -A first",
+        "./target/debug/ruwi -D -F src/parse/samples/iw_two_different_networks.txt -c print -s iw -A first",
         Some(200),
     )?;
     p.exp_string("[NOTE]: Selected network: \"Valparaiso_Guest_House 2\"")?;
@@ -26,7 +26,7 @@ fn test_iw_first_network_from_file() -> Result<()> {
     // assert_cmd would be nice for this type of stderr + stdout comparison, but it quadrupled
     // build times so we'll just use rexpect for now. The stdlib Command module would work as well.
     let mut p = spawn(
-        "./target/debug/ruwi -D -F src/samples/iw_two_different_networks.txt -c print -s iw -A first",
+        "./target/debug/ruwi -D -F src/parse/samples/iw_two_different_networks.txt -c print -s iw -A first",
         Some(200),
     )?;
     p.exp_string("[NOTE]: Selected network: \"Valparaiso_Guest_House 2\"")?;
